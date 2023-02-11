d0fb84d5b47c0fcc056b164be050ff2a0faac678
asaka <tuberoselilyllis@gmail.com>
2023-02-11 17:15:17 +0900

    カート機能、カート内一覧のトップへ戻るボタン、詳細画面からの変遷
    カート内一覧のトップへ戻るボタン
    詳細画面でカートへ入れるを押したときにまた同じ詳細画面のページが表示される

--- src/main/java/com/example/springbootsampleec/controllers/CartController.java
+++ src/main/java/com/example/springbootsampleec/controllers/CartController.java
@@ -21,90 +21,58 @@ public class CartController {
 	private final CartService cartService;
 	private final UserService userService;
 	private final ItemService itemService;
-	
-	public CartController(
-			CartService cartService,
-			UserService userService,
-			ItemService itemService
-		) {
+
+	public CartController(CartService cartService, UserService userService, ItemService itemService) {
 		this.cartService = cartService;
 		this.userService = userService;
 		this.itemService = itemService;
 	}
-	
-	@GetMapping("/")    
-    public String index(
-		@AuthenticationPrincipal(expression = "user") User user,
-		Model model) {
+
+	@GetMapping("/")
+	public String index(@AuthenticationPrincipal(expression = "user") User user, Model model) {
 		User refreshedUser = userService.findById(user.getId()).orElseThrow();
 		int totalAmount = refreshedUser.getTotalAmount();
-        model.addAttribute("title", "カート内の商品");
-        model.addAttribute("main", "carts/index::main");
-        model.addAttribute("user", refreshedUser);
-        model.addAttribute("totalAmount", totalAmount);
-        return "layout/logged_in";    
-    }
-	
-	@PostMapping("/add_cart/{id}")    
-    public String addCart(
-    	@PathVariable("id")  Long id,
-        RedirectAttributes redirectAttributes,
-        @AuthenticationPrincipal(expression = "user") User user,
-        Model model
-        ) {
-        cartService.addCart(
-            user,
-            id
-        );
-        redirectAttributes.addFlashAttribute(
-            "successMessage",
-            "カートに追加しました");
-        return "redirect:/items/";  
-    }
-	
+		model.addAttribute("title", "カート内の商品");
+		model.addAttribute("main", "carts/index::main");
+		model.addAttribute("user", refreshedUser);
+		model.addAttribute("totalAmount", totalAmount);
+		return "layout/logged_in";
+	}
+
+	@PostMapping("/add_cart/{id}")
+	public String addCart(@PathVariable("id") Long id, RedirectAttributes redirectAttributes,
+			@AuthenticationPrincipal(expression = "user") User user, Model model) {
+		cartService.addCart(user, id);
+		redirectAttributes.addFlashAttribute("successMessage", "カートに追加しました");
+		return "redirect:/items/";
+	}
+
 	/**
 	 * 詳細画面からもカートに入れる機能を追加
-	 * */
-	@PostMapping("/add_cart/detail/{id}")    
-    public String addCartDetail(
-    	@PathVariable("id")  Long id,
-        RedirectAttributes redirectAttributes,
-        @AuthenticationPrincipal(expression = "user") User user,
-        Model model
-        ) {
-        cartService.addCart(
-            user,
-            id
-        );
+	 */
+	@PostMapping("/add_cart/detail/{id}")
+	public String addCartDetail(@PathVariable("id") Long id, RedirectAttributes redirectAttributes,
+			@AuthenticationPrincipal(expression = "user") User user, Model model) {
+		cartService.addCart(user, id);
 		Item item = itemService.findById(id).orElseThrow();
 		model.addAttribute("item", item);
 		model.addAttribute("user", user);
 		model.addAttribute("title", "商品の詳細");
 		model.addAttribute("main", "items/detail::main");
-        redirectAttributes.addFlashAttribute(
-            "successMessage",
-            "カートに追加しました");
-        return "redirect:items/detail/{id}";  
-    }
+		redirectAttributes.addFlashAttribute("successMessage", "カートに追加しました");
+		return "redirect:/items/detail/" + id;
+	}
 
-	@PostMapping("/purchase")    
-    public String purchase(
-        RedirectAttributes redirectAttributes,
-        @AuthenticationPrincipal(expression = "user") User user,
-        Model model
-        ) {
-        if(cartService.purchase(user)) {
-        	redirectAttributes.addFlashAttribute(
-                "successMessage",
-                "商品を購入しました");
-        	return "redirect:/items/";  
-        } else {
-        	redirectAttributes.addFlashAttribute(
-                "errorMessage",
-                "商品を購入できませんでした。");
-        	return "redirect:/carts/"; 
-        }
-        
-        
-    }
+	@PostMapping("/purchase")
+	public String purchase(RedirectAttributes redirectAttributes,
+			@AuthenticationPrincipal(expression = "user") User user, Model model) {
+		if (cartService.purchase(user)) {
+			redirectAttributes.addFlashAttribute("successMessage", "商品を購入しました");
+			return "redirect:/items/";
+		} else {
+			redirectAttributes.addFlashAttribute("errorMessage", "商品を購入できませんでした。");
+			return "redirect:/carts/";
+		}
+
+	}
 }
